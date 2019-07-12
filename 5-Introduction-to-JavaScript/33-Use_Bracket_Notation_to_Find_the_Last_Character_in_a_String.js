97ec8ca4a31f569391441e63908c79d84d1227a7
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 14:36:31 -0600

    Find last character of lastName

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+/* We can get the last character of string using hard brakcets and .length property */
+
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
