718059d7949291dbb7d3e6fb88e68ea858a9e131
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 11:38:49 -0600

    Initialize and assign two new variables

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+//Strings are a set of characters inside a pair of single/double qoutes
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Isaac";
+var myLastName = "Ramirez";
