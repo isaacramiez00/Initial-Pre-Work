d73136ebe7dae73bd5d6cf0f86a5fed389509730
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 15:52:39 -0600

    Create timesFive()

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,15 @@
+//return statement sends a value back out of our function()
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+
+console.log(minusSeven(10));
+console.log(timesFive(3));
