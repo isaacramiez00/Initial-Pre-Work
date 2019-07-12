c76bbf5692559d022a0c379f6f1841719c0f4d2e
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 10:42:46 -0600

    Set remainder var value

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,7 @@
+//Remainder operator = %; returns the remaining value
+//% operator is useful to check if a value is even/odd
+//Refer to as modulus but doens not work properly with negative numbers
+
+// Only change code below this line
+
+var remainder = 11 % 3;
