b806b5e0bdcc5369b2e0b3a1851dc8b7f77f285c
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 09:22:58 -0600

    Create myName var

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,10 @@
+//data is anything meaningful to computer
+//datatypes: undefined, booleans, numbers, string
+//variable = store and manupulate data by using "labels" tp point to data 
+
+// Example
+var ourName;
+
+// Declare myName below this line
+
+var myName;
