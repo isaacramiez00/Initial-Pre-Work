9b551cf6c1d73e653dc9dcdf642ff2fc2de78d21
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 09:51:03 -0600

    Assign values to variables

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,13 @@
+//Initial var have a value of undefined until given one
+//Math operations result in "NaN" and undefined string var return "undefined"
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
