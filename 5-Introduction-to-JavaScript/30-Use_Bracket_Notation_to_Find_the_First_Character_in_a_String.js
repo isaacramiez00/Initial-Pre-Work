87bb6491b4b6b9fff335faeeedcb4f5a2881e245
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 14:09:32 -0600

    Get index 0 of lastName

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,15 @@
+//zero-based indexing is applied to javascript
+/* We can find the character of a specific index using bracket notation [] at the end of variable; myVar[i] */
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
