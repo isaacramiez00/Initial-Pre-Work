4fbc0a84b6842bffec2d8bbca234ada08e0bf857
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 17:08:25 -0600

    Create trueOrFalse()

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,26 @@
+//if statements is code that makes the decision, whether a condition is true or false
+//if statement is true, code inside curly bracket executes
+
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line.
+  if(wasThatTrue) {
+      return "Yes, that was true";
+  }
+  return "No, that was false";
+
+  // Only change code above this line.
+
+}
+
+// Change this value to test
+trueOrFalse(true);
