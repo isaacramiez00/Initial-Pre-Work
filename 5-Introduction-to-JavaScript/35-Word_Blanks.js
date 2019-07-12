77431f5ab874b7f54e55678128abf23c6cbd1f06
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 14:46:22 -0600

    Complete wordBlanks function

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,12 @@
+//Applied the concatenation operator to wordBlanks function
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "The " + myNoun + " was so " + myAdjective + ", that I " + myVerb + " " + myAdverb + ".";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("hippo", "big", "jumped", "quickly");
