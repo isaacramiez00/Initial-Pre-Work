ed5e1662f7572900ecf6ecf1ffd723b997ac6f56
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 15:00:18 -0600

    Set myData to value of 8

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,7 @@
+//When wanting to access multi-dimensional arrays, this first set of brackets are the outer-most and the next pair of brackets access the next level
+
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
