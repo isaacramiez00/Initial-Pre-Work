d79d9896eab58d153003c9870adaa1c8391f018d
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 14:13:23 -0600

    Update myStr var

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+//Individual character strings cannot be changed once created (immutable)
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Fix Me
