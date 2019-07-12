709ed947ab3d6503173130b4dd9b8a2ce0199329
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 14:52:48 -0600

    Assign a nested array to myArray var

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+//We can nest arrays; refered to as multi-dimensional Array
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["hello", "world"], [1, 2]];
