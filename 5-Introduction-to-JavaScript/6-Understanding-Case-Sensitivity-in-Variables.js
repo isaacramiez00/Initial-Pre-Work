f649a92f134e886dbeec7f3169601db7baaeb8c3
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 09:55:54 -0600

    Fix variable names

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,11 @@
+/*Javascript is case sensitive and best to practice camelCase when creating variables */
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
