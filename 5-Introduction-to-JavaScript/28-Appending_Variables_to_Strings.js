653e5f25598366f2691f930d06cd38c5dbf8cf5b
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 14:01:16 -0600

    Append someAdjective to myStr

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,13 @@
+//We can append variables using the += operator
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "Fun!";
+var myStr = "Learning to code is ";
+
+myStr += someAdjective;
