faeb74225b2e999e3f2bcde74a8fd55de1ebd92f
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 13:56:47 -0600

    Concatenate two str variables

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+//Learned how to concatenate variable inside a variable
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Isaac";
+var myStr = "My name is " + myName + " and I am well!";
