6255020eae831ce6814570edf6589702e11693d7
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 09:27:43 -0600

    Assign var a,b

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,10 @@
+// Assignment operator assigns a value to a var using the = symbol; right-to-left
+//right side is the variable, left side is the value 
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+a= 7;
+b = a;
