a1af6bc0e72af33d82605b0839c3ce7e315cae99
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 11:56:11 -0600

    Update myStr var

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,4 @@
+/*We can also use single or double qoutes in our string text as long as the
+outer qoutes differ and are not used inside the string */
+
+var myStr = '<a href= "http://www.example.com" target="_blank">Link</a>';
