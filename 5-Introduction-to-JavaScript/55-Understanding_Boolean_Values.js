9d3e8f92b7fa3d04436b0a0c25d7075d35f9fa7f
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 16:55:34 -0600

    Return true

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+//booleans are true/false statements; boolean values are not written in quotes
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
