e0f2100f18689b7152c6bd3a77feacf576730322
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 12:02:37 -0600

    Assign str to myStr var

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,4 @@
+//Learned about the different escape sequences in string
+// We must still use the \ escape character to dipslay a backslash: \\
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
