fbc6e1820aa119fc3c3265b226bc7726e9ee80de
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 16:10:58 -0600

    Create addFive()

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,16 @@
+//return statement not necessary but will return undefined if not called
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum += 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
