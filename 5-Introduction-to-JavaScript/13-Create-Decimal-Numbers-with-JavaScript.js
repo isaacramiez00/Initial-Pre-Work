c70b93d726a3623d5af800af9a40a026a6e04a4f
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 10:32:07 -0600

    Assign float value to myDecimal

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,7 @@
+//Decimal numbers are sometimes refered to as float
+//Not all real numbers can be data type float; Can lead to rounding errors
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = .33;
