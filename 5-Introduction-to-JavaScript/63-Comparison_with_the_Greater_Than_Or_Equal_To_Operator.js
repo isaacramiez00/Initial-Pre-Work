bdda46c07717318ff319e86c8b9b33d8aea1429e
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 21:32:06 -0600

    Add >= operator to if statements

---
+++ 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,16 @@
+//>= operator checks if left side value is greater than or equal to right side value
+
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
