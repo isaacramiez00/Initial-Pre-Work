8faf69cecd9b33ab0a61509b435efd0db299faa8
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 22:01:18 -0600

    Add multi if/else if statements

---
+++ 5-Introduction-to-JavaScript/71-Chaining_If_Else_Statements.js
@@ -0,0 +1,28 @@
+//We can write multi if/else if statements
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  }
+  else if (num < 10) {
+    return "Small";
+  }
+  else if (num < 15) {
+    return "Medium";
+  }
+  else if (num < 20) {
+    return "Large";
+  }
+  else if (num >= 20) {
+    return "Huge"
+  }
+  else {
+    return "Change Me";
+  }
+
+  // Only change code above this line
+}
+
+// Change this value to test
+testSize(10);
