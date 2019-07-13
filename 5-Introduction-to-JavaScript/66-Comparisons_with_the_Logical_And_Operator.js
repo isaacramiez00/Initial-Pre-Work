004b3fee852cadaff4b228398239a2b456fbebfa
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 21:41:44 -0600

    Update conditional statement

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,15 @@
+//logical and operator (&&) or nested if statements check if two statements are true
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val >= 25 && val <= 50) {
+    return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
