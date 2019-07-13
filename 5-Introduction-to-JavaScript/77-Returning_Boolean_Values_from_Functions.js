ce58b9313b63c13c77d616e81846edf2927dae96
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 23:37:58 -0600

    Return boolean value

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,11 @@
+// We can use comparison operator in our function to return boolean values!
+/* function isEqual(a,b) {
+  return a === b;
+}
+*/
+
+function isLess(a, b) {
+  return a < b;
+}
+// Change these values to test
+console.log(isLess(10, 15));
