2cb7c3382f2b4dec1d0109162bde0e0e165f57d2
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-12 21:46:00 -0600

    Use || operator for if statement

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,15 @@
+// The pipe symbol is used to express the logical or operator
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
