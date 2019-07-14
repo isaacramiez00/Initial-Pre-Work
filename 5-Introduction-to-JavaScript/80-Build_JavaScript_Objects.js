b523291fe3b13537e361296b626f58d191eed3f0
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-13 18:10:00 -1000

    Create an object to myDog var

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,25 @@
+//Objects are like arrays; instead of idx they are refered to as properties
+/* syntax:
+var cat = {
+  name: "Whisker", //javascript will automatically make the property a str;
+  "legs": 4
+  5: ["water", 2]
+};
+*/
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Isaac",
+  "legs": 2,
+  "tails": 0,
+  "friends": ["jack", "tanessa", "family"]
+};
