4f15646de71eabf94a3489554ca9c60e2e64a084
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-13 18:22:10 -1000

    Assign properties to entreeValue && drinkValue

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,24 @@
+//You need to use bracket notation [] if propertyName has a space
+//properties with space need quotes when accessing them through bracket notation
+/*
+var myObj = {
+  "Space Name": "Kirk",
+  "More Space": "Spock",
+  "NoSpace": "USS Enterprise"
+};
+myObj["Space Name"]; // Kirk
+myObj['More Space']; // Spock
+myObj["NoSpace"]; // USS Enterprise
+*/
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
