c4d39bc232b184abc38cb6735686c8b5198ae92e
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-14 01:18:19 -1000

    Create updateRecords() to modify album collections

---
+++ 5-Introduction-to-JavaScript/92-Record_Collection.js
@@ -0,0 +1,55 @@
+//I learned that we can check if the object properties are present (true) by just calling the object;
+//snytax: if (collection[id][prop]) //results in either true/false
+
+// Setup
+var collection = {
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [
+        "Let It Rock",
+        "You Give Love a Bad Name"
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [
+        "1999",
+        "Little Red Corvette"
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+    "5439": {
+      "album": "ABBA Gold"
+    }
+};
+// Keep a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+// Only change code below this line
+function updateRecords(id, prop, value) {
+  if (prop === "tracks" && value !== "") {
+   if(collection[id][prop]) {
+    collection[id][prop].push(value); //update array
+   }
+   else {
+    collection[id][prop]=[value]; //create emtpy array
+   }
+  }
+  else if (value !== "") {
+    collection[id][prop] = value;
+  }
+  else {
+    delete collection[id][prop];
+  }
+
+  return collection;
+}
+
+
+// Alter values below to test your code
+updateRecords(5439, "artist", "ABBA");
