98ba6f55225482b3d0e1504e945c0db4ae84d8f4
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-13 18:16:23 -1000

    Set property values to var hatValue && shirtValue

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,23 @@
+//You can access the properties of an object using dot notation ( . ) or bracket notation []
+//dot notation are best used if you know the property name; varObjectName.propertyName
+/* var myObj = {
+  prop1: "val1",
+  prop2: "val2"
+};
+var prop1val = myObj.prop1; // val1
+var prop2val = myObj.prop2; // val2
+*/
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
+
+console.log(hatValue);
