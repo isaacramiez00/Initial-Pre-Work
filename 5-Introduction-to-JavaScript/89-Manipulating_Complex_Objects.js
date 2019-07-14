4f3cc5c20c9938d164cefd2d8741489dacb83549
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-13 19:56:53 -1000

    Add album to myMusic array

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,28 @@
+//Objects are the prefer way to handle multi data types data structure; flexible data
+//"JavaScript Object Notation (JSON) is related data interchange format used to store data" (freeCodeCamp)
+//objects have a key:value format
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Odesza",
+    "title": "A Moment Apart",
+    "release_year": 2018,
+    "formats": [
+      "itunes",
+      "Vinyl",
+      "CD"
+    ]
+  }
+  // Add record here
+];
