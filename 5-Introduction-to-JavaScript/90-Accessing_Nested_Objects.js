4b2ae94950f648df9e16ba9b8d3b89eb4de1231f
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-13 20:05:54 -1000

    Assign "glove box" property value to gloveBoxContents

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+//We can access nested properties by continually adding dot notation or bracket notation; ObjectName.property.subProperty
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
