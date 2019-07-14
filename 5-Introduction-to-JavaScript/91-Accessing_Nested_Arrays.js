3b38efd528dd04600ce85e36e9e28ea91667a8de
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-13 20:14:07 -1000

    Assign value to secondTree var

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,26 @@
+//We can access nested arrays with objects the same way we do accessing nested objects
+// arrayName.index[i].index[i]
+
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
