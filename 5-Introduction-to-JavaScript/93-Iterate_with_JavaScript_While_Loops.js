d6adeedfb700bc3c175a949c2dc7d235cbaf1a40
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-14 15:53:25 -1000

    Push values to myArray

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,12 @@
+//while loops repeat code until condition results to false;
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i < 5) {
+  myArray.push(i);
+  i++;
+}
+console.log(myArray)
