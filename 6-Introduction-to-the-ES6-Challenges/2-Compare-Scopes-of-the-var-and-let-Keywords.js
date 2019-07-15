dac49bac8d4d1bb7f2acbcecbfbe4fd9e51ed04f
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-15 12:08:21 -1000

    Keep i vars inside && outside if statement const

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,15 @@
+//let keywords inside blocks/statements are limited to that respective scope
+
+
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+
+console.log(checkScope());
