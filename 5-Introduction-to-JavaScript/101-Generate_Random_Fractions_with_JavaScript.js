619c9155ee1fc22f153ead6d8a06a39ef8bbb439
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-14 22:32:46 -1000

    Change randomFraction() to return random number

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,12 @@
+//Math.random() function returns a number between 0(inclusive) and 1(exclusive)
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random(10);
+
+  // Only change code above this line.
+}
+
+//console.log(randomFraction())
