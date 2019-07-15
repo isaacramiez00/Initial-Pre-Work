c0a4725bce1b60267119fbe24c640b14bfdb73c7
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-14 16:09:17 -1000

    Push decrementing odd values to myArray

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,16 @@
+//We can decrament values using for loops
 
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for(i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
