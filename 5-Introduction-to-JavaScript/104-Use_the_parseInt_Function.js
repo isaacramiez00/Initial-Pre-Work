fbdd35740584bb1fc2c28dee3f8495423bd167c2
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-14 23:06:52 -1000

    Create convertToInteger() to convert str arg to int

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,8 @@
+//parseInt("num"); converts a string to an integer
+//if 1st character in str can't be converted then it returns NaN
+
+function convertToInteger(str) {
+  return parseInt(str);
+}
+
+console.log(convertToInteger("56"));
