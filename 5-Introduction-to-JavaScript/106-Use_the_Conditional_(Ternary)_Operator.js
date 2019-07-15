326836b24aae1ffb8eca2d41a6ac5a920880cd03
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-14 23:20:21 -1000

    Check if int arg are equal or not in checkEqual()

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,13 @@
+//Conditional operator or ternary operator is a one line if/else statement
+//syntax: condition ? statement-if-true : statement-if-false;
+/*
+function findGreater(a, b) {
+  return a > b ? "a is greater" : "b is greater";
+}
+*/
+
+function checkEqual(a, b) {
+  return a == b ? true : false;
+}
+
+console.log(checkEqual(2, 2));
