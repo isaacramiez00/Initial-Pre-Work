cf058cf33fc62542219aa26b36a0c49804e8cd8f
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-14 15:58:09 -1000

    Push values to myArray

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,17 @@
+//for loop is the other type of repeat function
+//syntx: for ([initialization]; [condition]; [final-expression])
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 6; i++) {
+    myArray.push(i);
+}
