a37aee199750335872953edbbbf52df84077c384
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-15 17:12:59 -1000

    Ensure MATH_CONSTANTS is inmutable

---
+++ 6-Introduction-to-the-ES6-Challenges/5-Prevent-Object-Mutation.js
@@ -0,0 +1,30 @@
+//We can ensure objects may not be modify the object in anyway by using the Object.freeze(objectName) function;
+/*
+let obj = {
+  name:"FreeCodeCamp",
+  review:"Awesome"
+};
+Object.freeze(obj);
+obj.review = "bad"; //will be ignored. Mutation not allowed
+obj.newProp = "Test"; // will be ignored. Mutation not allowed
+console.log(obj);
+// { name: "FreeCodeCamp", review:"Awesome"}
+*/
+
+function freezeObj() {
+  "use strict";
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // change code below this line
+  Object.freeze(MATH_CONSTANTS);
+
+  // change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch( ex ) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
