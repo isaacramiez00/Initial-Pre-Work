5d295af9b3c8e703c06b05f8ae045bae25b2aeee
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-15 16:33:15 -1000

    Declare const and let var

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,18 @@
+//const keyword is like let but once assigned, you cannot reassign
+//const variables should be in all caps
+
+
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
