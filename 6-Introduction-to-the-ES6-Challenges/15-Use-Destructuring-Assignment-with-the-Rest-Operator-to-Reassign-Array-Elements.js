0ed5ae852addd2d82aa741a225d1613bba48b2d4
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-19 11:38:24 -1000

    destruct source array and assign to arr variable

---
+++ 6-Introduction-to-the-ES6-Challenges/15-Use-Destructuring-Assignment-with-the-Rest-Operator-to-Reassign-Array-Elements.js
@@ -0,0 +1,16 @@
+/* We can use rest operators when destructuring assignments;
+the rest operator can only be applied as the last variable in the list:
+[a, b, ...arg] = arrayList //the first two elements will be assigned to var a and b
+*/
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  const [a, b, ...arr] = list; // change this
+  // change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
