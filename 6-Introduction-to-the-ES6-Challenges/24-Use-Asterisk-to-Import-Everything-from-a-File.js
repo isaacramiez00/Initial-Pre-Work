7cafae751ac620d8b94a4ac11917c700b697c7c9
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-19 16:37:01 -1000

    Import all functionality from "capitalize_strings" file

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,9 @@
+//We can import all functionality from a file using the * symbol
+/* syntax:
+import * as object_with_name_of_your_choice from "file_path_goes_here"
+
+object_with_name_of_your_choice.imported_function //to call and use imported_function
+*/
+
+"use strict";
+import * as bob from "capitalize_strings";
