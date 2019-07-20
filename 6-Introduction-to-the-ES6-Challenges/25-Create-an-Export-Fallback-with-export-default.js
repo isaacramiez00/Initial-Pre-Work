584dd313a715ce3eff92920cbecddf4d4c092b11
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-19 16:43:17 -1000

    Export default subtract()

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,6 @@
+//export default statement is used for fallback value for a file/module
+//Is used only(and can only be used once) if one value is being exported; var, let, and const statement are not supported with export default
+
+
+"use strict";
+export default function subtract(x,y) {return x - y;}
