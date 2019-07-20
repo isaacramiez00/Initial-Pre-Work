8d689a4b46be10f782a28563ce9a90ce0d5f6b7e
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-07-19 16:48:16 -1000

    Import subtract from "math_functions"

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,6 @@
+//If we use the export default statement in another file, we can import a value without using curly brackets {}
+//Other than that, same syntax
+
+"use strict";
+import subtract from "math_functions";
+subtract(7,4);
